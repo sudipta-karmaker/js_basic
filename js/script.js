5986c49f7c677a44e41d083db4dd151771aa35d0
sudipta-karmaker <sudipta.kmker@gmail.com>
2026-07-08 12:20:37 +0600

    var,let,const

---
+++ js/script.js
@@ -0,0 +1,44 @@
+alert("Welcome")
+var x=30
+console.log(x);
+
+var x=50
+var x=70
+console.log(x);
+
+var x=55
+var y=66
+console.log(x,y);
+console.log(x+y);
+
+var x="sudipta"
+console.log(x,"khub valo");
+
+
+let z="uthso"
+z= "sk"
+console.log(z);
+
+{
+
+    var x="mine"
+
+}
+console.log(x);
+
+{
+    let y="uthso"
+
+    console.log(y);
+    
+}
+
+{
+    const pi=3.1416
+
+    console.log(pi);
+    
+}
+
+
+
